f7758c2c1119cf19cd557a36693f4a5f70664089
Kyeorae Yeom <duarufp06@gmail.com>
2023-11-14 15:37:14 +0900

    change targetTime

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Timer from "@/components/timer"
 export default function Home() {
   return (
     <main className="w-full h-full flex justify-center items-center">
-      <Timer targetTime={new Date('2023-11-24 20:00:00').getTime()} />
+      <Timer targetTime={new Date('2023-11-24T20:00:00Z').getTime()} />
     </main>
   )
 }
